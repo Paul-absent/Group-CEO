590729f64a71d690029a0075fd1f81aab46bf42f
Paul-absent <rajupaul120@gmail.com>
2025-06-30 16:07:25 +0100

    1st change for Tina
    this is a group CEP code

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 print("hello world")
 print(4*8)
+
+for loopCounter in range(5,21,1):
+    counterIsEven = (loopCounter % 2) == 0
+    if counterIsEven:
+        print(f'{loopCounter} is even')
+    else:
+        print(f'{loopCounter} is odd')
